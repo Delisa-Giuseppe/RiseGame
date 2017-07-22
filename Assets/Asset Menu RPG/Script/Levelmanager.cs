f08f48a82bf8ce32968e399575135871cc78980c
Vincenzo Villani <v.villani84@alice.it>
2017-07-22 11:12:04 +0200

    Aggiunto il menu principale

---
+++ Assets/Asset Menu RPG/Script/Levelmanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levelmanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void LoadLevel (string name){
+		Debug.Log("Level load request for:"+name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void LoadByIndex (int sceneIndex){
+		Debug.Log("Level load request for:"+sceneIndex);
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void Quit(){
+//#if UNITY_EDITOR
+	//UnityEditor.EditorApplication.isPlaying = false;
+//#else
+	//Application.Quit();
+//#endif 
+		Application.Quit() ; 
+		Debug.Log("Quit request");
+
+	}
+
+}
---
+++ Assets/Asset Menu RPG/Script/Personaggi.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Personaggi : MonoBehaviour {
+
+    public Button A;
+    public Button B;
+    public int party;
+
+	// Use this for initialization
+	void Start () {
+        party = 0;
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        if (party >= 4)
+        {
+            sblocco();
+        }
+        else {
+            A.interactable = false;
+            B.interactable = false;
+        }
+    }
+
+    public void sblocco() {
+       
+            A.interactable = true;
+            B.interactable = true;
+            Debug.Log("Partiamo"); 
+    }
+
+    public void reset()
+    {
+        party = 0;
+    }
+
+
+    public void ImIn() {
+        party++;
+        Debug.Log(party);
+    }
+}
