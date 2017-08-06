90675217118f12c88c70bef118e72e7775e0d17b
Vincenzo Villani <v.villani84@alice.it>
2017-08-06 19:31:19 +0200

    Added ui button image and ability image

--- Assets/Scripts/Ability/Thief/RitornoPlanare.cs
+++ Assets/Scripts/Ability/Thief/RitornoPlanare.cs
@@ -6,21 +6,52 @@ using UnityEngine.UI;
 
 public class RitornoPlanare : Ability {
 
+	private int currentMoves;
+
 	// Use this for initialization
 	void Start ()
 	{
 		this.abilityName = "RitornoPlanare";
+		currentMoves = GetComponent<PlayerController> ().moves; 
 		this.damage = 0;
 		this.cure = 0;
 		this.tileRange = 2;
 		this.cooldown = 4;
         countCooldown = this.cooldown;
+		this.turnDuration = 2;
+		countTurnDuration = this.turnDuration;
         playerUI = GetComponent<PlayerController>().playerUI;
         buttonPlayerUI = playerUI.GetComponentsInChildren<Button>()[2];
         buttonPlayerUI.onClick.AddListener(delegate {
-            AttivaAbilita(SelectType.ROMBO);
-            activedAbility = this.abilityName;
+			TileManager.ResetGrid ();
+			StartCoroutine (Wait(0.5f));
+			activedAbility = this.abilityName;
         });
+	}
+
+	IEnumerator Wait(float delay)
+	{
+		GameManager.currentState = GameManager.States.ABILITY;
+		yield return new WaitForSeconds(delay);
+
+		TileManager.tilesSelectable.Add(GetComponent<PlayerController>().PlayerTile);
+		GetComponent<PlayerController>().PlayerTile.GetComponent<SpriteRenderer>().color = Color.green;
+
+		UsaAbilita ();
+	}
+
+	public override void UsaAbilita()
+	{
+		GetComponent<PlayerController> ().moves += 2; 
+		AddAbilityToTurnDurationList(this);
+		StartCoroutine(TileManager.WaitMoves(this.gameObject, GameManager.States.END_MOVE));
+	}
+
+	public override void ResettaValori()
+	{
+		PlayerController controller = GetComponent <PlayerController> ();
+		controller.moves = currentMoves;
+		controller.CalculateStatistics ();
 	}
 
 }
